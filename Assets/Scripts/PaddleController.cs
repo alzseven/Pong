48526a4908820b37e2a20606ac7dbd8480970056
alz7 <alskdjfhg7455@naver.com>
2020-06-27 16:32:34 +0900

    first draft

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pong{
+    public class PaddleController : MonoBehaviour
+    {
+        [SerializeField]
+        private float velocity = 5.0f;
+        [SerializeField]
+        private string axisName = "Vertical";
+        [SerializeField]
+        private Rigidbody2D rb;
+
+        // Update is called once per frame
+        void Update()
+        {
+            var dir = Input.GetAxisRaw(axisName);
+            rb.velocity = new Vector2(rb.velocity.x, dir * velocity );
+        }
+    }
+}
+
